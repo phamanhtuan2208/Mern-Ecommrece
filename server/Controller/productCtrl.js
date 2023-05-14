849910a22dfab37c07d476d36ea1cf41085b6efe
AnhTuan <tuantony2208@gmail.com>
2023-05-14 16:01:50 +0700

    Final API Product

--- server/Controller/productCtrl.js
+++ server/Controller/productCtrl.js
@@ -44,8 +44,9 @@ const deleteProduct = AsyncHandler(async (req, res) => {
 //get all product
 const getAllProduct = AsyncHandler(async (req, res) => {
     try {
-        //
         const queryObject = { ...req.query };
+
+        //filter price gte|gt|lte|lt
         const excludeFields = ['page', 'sort', 'limit', 'fields'];
         excludeFields.forEach((el) => delete queryObject[el]);
         let queryString = JSON.stringify(queryObject);
@@ -53,10 +54,37 @@ const getAllProduct = AsyncHandler(async (req, res) => {
             /\b(gte|gt|lte|lt)\b/g,
             (match) => `$${match}`,
         );
-        console.log(JSON.parse(queryString));
-        //
-        const findAllProduct = await Product.find(queryObject);
-        res.json(findAllProduct);
+        let query = Product.find(JSON.parse(queryString));
+
+        //Sorting
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort('-createdAt');
+        }
+
+        //limiting the field
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields);
+        } else {
+            query = query.select('-__v');
+        }
+
+        //pagination
+        const page = req.query.page;
+        const limit = req.query.limit;
+        const skip = (page - 1) * limit;
+        query = query.skip(skip).limit(limit);
+        if (req.query.page) {
+            const productCount = await Product.countDocuments();
+            if (skip >= productCount)
+                throw new Error('This Page does not exists');
+        }
+        
+        const product = await query;
+        res.json(product);
     } catch (error) {
         throw new Error(error);
     }
