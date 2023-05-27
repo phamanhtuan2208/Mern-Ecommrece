e68ff612fd0c1fd1c5f94bea76b06a5a98569737
AnhTuan <tuantony2208@gmail.com>
2023-05-27 20:36:12 +0700

    Home #2

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { BsLinkedin, BsGithub, BsYoutube, BsInstagram } from 'react-icons/bs';
 
 const Footer = () => {
     return (
@@ -18,7 +20,7 @@ const Footer = () => {
                             </div>
                         </div>
                         <div className="col-7">
-                            <div className="input-group">
+                            <div className="input-group py-1">
                                 <input
                                     type="text"
                                     className="form-control py-1"
@@ -37,7 +39,99 @@ const Footer = () => {
                     </div>
                 </div>
             </footer>
-            <footer className="py-3"></footer>
+            <footer className="py-4">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-4">
+                            <h4 className="text-white mb-4">Contact Us</h4>
+                            <div>
+                                <address className="text-white fs-6">
+                                    Hno: 277 Near Vill chopal,
+                                    <br /> Soipat, Haryana <br />
+                                    PinCode: 131103
+                                </address>
+                                <a
+                                    href="tel: +91 9264099243"
+                                    className="mt-3 d-block mb-1 text-white"
+                                >
+                                    +91 9264099243
+                                </a>
+                                <a
+                                    href="mailto: AT@gmail.com"
+                                    className="mt-2 d-block mb-0 text-white"
+                                >
+                                    AT@gmail.com
+                                </a>
+                                <div className="social_icons d-flex align-items-center gap-30 mt-4">
+                                    <a className='text-white' href="">
+                                        <BsLinkedin className='text-white fs-4'></BsLinkedin>
+                                    </a>
+                                    <a className='text-white' href="">
+                                        <BsInstagram className='text-white fs-4'></BsInstagram>
+                                    </a>
+                                    <a className='text-white' href="">
+                                        <BsGithub className='text-white fs-4'></BsGithub>
+                                    </a>
+                                    <a className='text-white' href="">
+                                        <BsYoutube className='text-white fs-4'></BsYoutube>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <h4 className="text-white mb-4">Information</h4>
+                            <div className="footer-links d-flex flex-column">
+                                <Link className="text-white py-3 mb-1">
+                                    Privacy Policy
+                                </Link>
+                                <Link className="text-white py-3 mb-1">
+                                    Refund Policy
+                                </Link>
+                                <Link className="text-white py-3 mb-1">
+                                    Shipping Policy
+                                </Link>
+                                <Link className="text-white py-3 mb-1">
+                                    Terms & Conditions
+                                </Link>
+                                <Link className="text-white py-3 mb-1">
+                                    Blogs
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="col-3">
+                            <h4 className="text-white mb-4">Account</h4>
+                            <div className="footer-links d-flex flex-column">
+                                <Link className="text-white py-3 mb-1">
+                                    About Us
+                                </Link>
+                                <Link className="text-white py-3 mb-1">
+                                    Faq
+                                </Link>
+                                <Link className="text-white py-3 mb-1">
+                                    Contact
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="col-2">
+                            <h4 className="text-white mb-4">Quick Links</h4>
+                            <div className="footer-links d-flex flex-column">
+                                <Link className="text-white py-3 mb-1">
+                                    Laptop
+                                </Link>
+                                <Link className="text-white py-3 mb-1">
+                                    Headphones
+                                </Link>
+                                <Link className="text-white py-3 mb-1">
+                                    Tablet
+                                </Link>
+                                <Link className="text-white py-3 mb-1">
+                                    Watch
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
             <footer className="py-4">
                 <div className="container-xxl">
                     <div className="row">
