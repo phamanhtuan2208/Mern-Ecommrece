3b41f607d75927ece9014b83c5deb1d737929a7d
AnhTuan <tuantony2208@gmail.com>
2023-08-03 10:40:30 +0700

    API Client #1 (Service and Slice Register)

---
+++ client/src/features/User/userSlice.js
@@ -0,0 +1,46 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { authService } from './userService';
+
+export const registerUser = createAsyncThunk(
+    'auth/register',
+    async (userData, thunkAPI) => {
+        try {
+            return await authService.register(userData);
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error);
+        }
+    },
+);
+
+const initialState = {
+    user: '',
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: '',
+};
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState: initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(registerUser.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(registerUser.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+                state.isError = false;
+            })
+            .addCase(registerUser.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = false;
+                state.isError = true;
+                state.message = action.error;
+            });
+    },
+});
+
+export default authSlice.reducer;
